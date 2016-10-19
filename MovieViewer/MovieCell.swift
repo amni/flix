28c65b4dc53d24c9381677f79cd2cef2e4a69377
amni <amni2015@gmail.com>
2016-10-18 18:38:37 -0700

    initial working prototype

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Alan Ni on 10/18/16.
+//  Copyright © 2016 Alan Ni. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+}
